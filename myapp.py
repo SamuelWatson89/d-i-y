4ecfa5b560f892b52a46e20b1bbaf37b7e817216
Samuel <otacon100@hotmail.com>
2019-12-09 21:02:03 +0000

    Added more test to check more endpoints

--- myapp.py
+++ myapp.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, redirect, url_for
+
 
 def create_app():
     app = Flask(__name__)
@@ -7,10 +8,37 @@ def create_app():
     def get_projects():
         return jsonify(template='projects.html')
 
-    
     @app.route('/login')
     def login():
         return jsonify(template='login.html')
+
+    @app.route("/signup")
+    def signup():
+        return jsonify(template='signup.html')
+
+    @app.route("/add_project")
+    def add_project():
+        return jsonify(template='addproject.html')
+
+    @app.route('/view_project/<projects_id>')
+    def view_project(projects_id):
+        return jsonify(template='viewproject.html')
+
+    @app.route("/edit_project/<projects_id>")
+    def edit_projects(projects_id):
+        return jsonify(template='editproject.html')
+
+    @app.route('/edit_image/<projects_id>')
+    def edit_image(projects_id):
+        return jsonify(template='editimage.html')
+
+    @app.route("/about")
+    def about():
+        return jsonify(template='about.html')
+
+    @app.route("/contact")
+    def contact():
+        return jsonify(template='contact.html')
 
     return app
 
--- test_app.py
+++ test_app.py
@@ -1,6 +1,9 @@
 import pytest
 from flask import url_for
 
+from app import get_projects
+
+
 class TestApp:
 
     def test_get_projects(self, client):
@@ -12,3 +15,38 @@ class TestApp:
         res = client.get(url_for('login'))
         assert res.status_code == 200
         assert res.json == {'template': 'login.html'}
+
+    def test_signup(self, client):
+        res = client.get(url_for('signup'))
+        assert res.status_code == 200
+        assert res.json == {'template': 'signup.html'}
+
+    def test_add_project(self, client):
+        res = client.get(url_for('add_project'))
+        assert res.status_code == 200
+        assert res.json == {'template': 'addproject.html'}
+
+    def test_view_project(self, client):
+        res = client.get(url_for('view_project', projects_id='1'))
+        assert res.status_code == 200
+        assert res.json == {'template': 'viewproject.html'}
+
+    def test_edit_projects(self, client):
+        res = client.get(url_for('edit_projects', projects_id='1'))
+        assert res.status_code == 200
+        assert res.json == {'template': 'editproject.html'}
+
+    def test_edit_image(self, client):
+        res = client.get(url_for('edit_image', projects_id='1'))
+        assert res.status_code == 200
+        assert res.json == {'template': 'editimage.html'}
+
+    def test_about(self, client):
+        res = client.get(url_for('about'))
+        assert res.status_code == 200
+        assert res.json == {'template': 'about.html'}
+
+    def test_contact(self, client):
+        res = client.get(url_for('contact'))
+        assert res.status_code == 200
+        assert res.json == {'template': 'contact.html'}
